1a14f7d3b0db3e929f613e11d871d9100b915726
justinnp <31677589+justinnp@users.noreply.github.com>
2018-12-08 12:22:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SporSight Athlete Technique Analysis</h1>
+        <h1>SporSight Athlete Technique Analysis Senior Design</h1>
       </div>
     );
   }

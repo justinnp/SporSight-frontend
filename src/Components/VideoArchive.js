7f0f9d048c68566d56aeb2f691b86cf54b7f259f
Amaan Chowdhury <amaanc96@Amaans-MacBook-Pro.local>
2019-01-10 15:22:55 -0500

    changed variable videoName

--- src/Components/VideoArchive.js
+++ src/Components/VideoArchive.js
@@ -2,43 +2,44 @@ import React, { Component } from "react";
 import { Row, Button } from "reactstrap";
 import Topbar from "./Topbar";
 import Sidebar from "./Sidebar";
-var videoName = '07d2d2230c2b7d4b1da05ec3b414f62d';
-const url = 'http://localhost:3001'
+var videoName = "410b6c6df23ff91fc9c9ba8667fbf91e";
+const url = "http://localhost:3001";
 
 class VideoArchive extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      videoUrl: '',
+      videoUrl: "",
       retrievedVideo: false
-    }
+    };
   }
 
   renderVideo() {
-    if(this.state.retrievedVideo){
+    if (this.state.retrievedVideo) {
+      console.log("video url is: " + this.state.videoUrl);
+
       return (
-        <video width="100%" controls >
+        <video width="100%" controls>
           <source src={this.state.videoUrl} type="video/mp4" />
         </video>
-      )
+      );
     }
   }
 
   fetchVideo(param) {
     fetch(url + `/api/video_archive/${param}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.sasUrl);
-      this.setState({
-        videoUrl: data.sasUrl,
-        retrievedVideo: true
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.sasUrl);
+        this.setState({
+          videoUrl: data.sasUrl,
+          retrievedVideo: true
+        });
+      })
+      .catch(error => {
+        console.log("Error in retrieving video", error);
       });
-    })
-    .catch(error => {
-      console.log('Error in retrieving video', error);
-  });
   }
-
 
   render() {
     return (
@@ -52,13 +53,13 @@ class VideoArchive extends Component {
           </div>
           <div class="col offset-md-2 displayArea">
             <h2>Video Archive</h2>
-            <Button onClick={() => this.fetchVideo(videoName)}>Show Video</Button>
+            <Button onClick={() => this.fetchVideo(videoName)}>
+              Show Video
+            </Button>
           </div>
         </Row>
         <Row id="body-row">
-          <div class="col offset-md-2">
-            {this.renderVideo()}
-          </div>
+          <div class="col offset-md-2">{this.renderVideo()}</div>
         </Row>
       </div>
     );
